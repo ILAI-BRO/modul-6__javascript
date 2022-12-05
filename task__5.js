b724f0221121509863cd7974fb98576905bb3290
ILYAS <41228178+ILAI-BRO@users.noreply.github.com>
2022-12-05 12:37:41 +0300

    Update task__5.js

--- task__5.js
+++ task__5.js
@@ -1,19 +1,17 @@
-function number(x, b) {
-  let result = x;
+function num(x, b) {
+  let res = x;
 
   for (let i = 1; i < b; i++) {
-    result *= x;
+    res *= x;
   }
 
-  return result;
+  return res;
   
 }
 
 let x = prompt("задайте число x", '');
 let b = prompt("задайте число b", '');
 
-if (b < 1) {
-  console.log(`Степень ${b} не поддерживается, используйте натуральное число`);
-} else {
-  console.log( number(x, b) );
-}
+
+console.log( num(x, b) );
+
