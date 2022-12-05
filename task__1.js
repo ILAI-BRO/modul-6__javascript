c5dbf31c82c51ead53b119f0d0f19736eaf98949
ILYAS <41228178+ILAI-BRO@users.noreply.github.com>
2022-12-05 11:25:47 +0300

    Create task__1.js

---
+++ task__1.js
@@ -0,0 +1,23 @@
+function isSumm(number) {
+  
+    let BolTrue = 'число простое';
+    let BolFalse = 'Ответ не верный';
+   
+  
+  
+   if (number == 2) return BolTrue;
+   if (number < 1 && number > 1000) return "Ошибка";
+  
+  
+    for (let i = 2; i <= number / 2; i++) {
+   
+    if (number % i == 0) {
+      return BolFalse;
+    }
+      
+    }
+    return BolTrue;
+    }
+
+console.log(isSumm(1001));
+console.log(isSumm(23));
